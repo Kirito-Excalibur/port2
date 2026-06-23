b9518f33690588efc551a83ef80469af806c18fd
Nikhil Simon Toppo <nikhilsimontoppo@gmail.com>
2026-06-23 22:19:57 +0530

    Init:V3

--- extraterrestrial-ephemera/src/consts.ts
+++ extraterrestrial-ephemera/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Kirito';
+export const SITE_DESCRIPTION = 'Homage to the Lord Of Vows and New Beginning! One who tries to be as professional as one can be!';
